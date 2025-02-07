3bb73ae625a536053067431ed7f99a1b46505c39
Rohanyadav143 <rohn1895@gmail.com>
2025-02-07 10:16:25 +0530

    index Section Creataed

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Portfolio-Website
-
 <br>
-
-Abc
+This is Simple Portfolio-Website which contains all about me.
+<br>
+My Name is <b>Rohan yadav</b>
+<br>
+Student of Engineering College R.R Group Of Institution.
+<br>
+B.Tech with CSE
---
+++ index.js
@@ -0,0 +1,24 @@
+const words = ["Student", "Frontend-Developer", "Java-Full-Stack-Developer"];
+let wordIndex = 0;
+let charIndex = 0;
+let displayText = document.getElementById("message");
+
+function typeCharacter() {
+    if (charIndex < words[wordIndex].length) {
+        displayText.innerHTML += words[wordIndex][charIndex];
+        charIndex++;
+        setTimeout(typeCharacter, 200);
+    } 
+    else {
+        setTimeout(clearAndNextWord, 1000);
+    }
+}
+
+function clearAndNextWord() {
+    charIndex = 0;
+    displayText.innerHTML = "";
+    wordIndex = (wordIndex + 1) % words.length;
+    setTimeout(typeCharacter, 500);
+}
+
+typeCharacter();
